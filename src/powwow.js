c7db209999803eff4d8cc539074bd704e2167c5d
Jose Badeau <Jose Badeau@JoseBadeau-PC>
2014-08-08 16:07:34 +0200

    .

--- src/powwow.js
+++ src/powwow.js
@@ -1,5 +1,16 @@
-var powwow = {};
-
-powwow.VERSION = '0.1.0';
-
-export default powwow;
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	}
+	else if (typeof exports === 'object') {
+		module.exports = factory();
+	}
+	else {
+		root.returnExports = factory();
+	}
+}(this, function() {
+	var powwow = {
+		VERSION : '0.1.0'
+	};
+	return powwow;
+}));
