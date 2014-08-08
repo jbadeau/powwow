59f5f160e107349b0055ff61c514903fa8d3cd7c
Jose Badeau <Jose Badeau@JoseBadeau-PC>
2014-08-08 13:38:49 +0200

    added polymer pw-widget component

--- src/powwow.js
+++ src/powwow.js
@@ -1,20 +1,5 @@
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([], factory);
-    } else if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.returnExports = factory();
-  }
-}(this, function () {
-	var powwow = {};
+var powwow = {};
 
-	powwow.VERSION = '0.1.0';
+powwow.VERSION = '0.1.0';
 
-	return powwow;
-}));
+export default powwow;
