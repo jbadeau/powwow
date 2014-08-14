9a9ef66e9af08476681c93595b3291e729d8b46b
t229545 <t229545@W01B6X2Q.UBSPROD.MSAD.UBS.NET>
2014-08-14 08:52:56 +0200

    .

--- demo/app-bus.js
+++ demo/app-bus.js
@@ -1,34 +1,49 @@
 define(function(require) {
-	
-	var msgs = require('msgs');
-	
-	var bus = msgs.bus();
-	
-	var clientBus1 = msgs.bus();
-	
-	var clientBus2 = msgs.bus();
-	
-	var inbound = bus.channel('inbound');
 
-	var outbound = bus.channel('outbound');
-	
+	var msgs, exchangeDispatcher, unicastDispatcher, bus;
+
+	msgs = require('msgs/channels/exchange');
+
+	exchangeDispatcher = require('msgs/channels/dispatchers/exchange');
+
+	unicastDispatcher = require('msgs/channels/dispatchers/unicast');
+
+	hubBus = msgs.bus();
+
+	clientBus1 = hubBus.bus();
+	clientBus2 = hubBus.bus();
+	clientBus3 = hubBus.bus();
+
+	var listener = {
+		handle : function(message) {
+			console.debug('listener: ' + JSON.stringify(message));
+		}
+	}
 	var listener1 = {
 		handle : function(message) {
-			alert(1);
-			console.debug(message);
+			console.debug('listener1: ' + JSON.stringify(message));
 		}
-	};
-
+	}
 	var listener2 = {
-			handle : function(message) {
-				alert(1);
-				console.debug(message);
-			}
-		};
+		handle : function(message) {
+			console.debug('listener2: ' + JSON.stringify(message));
+		}
+	}
+	var listener3 = {
+		handle : function(message) {
+			console.debug('listener3: ' + JSON.stringify(message));
+		}
+	}
+
+	hubBus.topicExchangeChannel('outbound');
+
+	clientBus1.subscribe('outbound!greeting.#', listener1);
+	clientBus2.subscribe('outbound!greeting.en.*', listener2);
+	clientBus3.subscribe('outbound!#.fr.#', listener3);
 	
-	clientBus1.subscribe(outbound, listener1);
-	clientBus2.subscribe(outbound, listener2);
+	clientBus2.unsubscribe('outbound', listener2);
 	
-	bus.send(outbound, 'hello world');
-	
+	hubBus.send('outbound!greeting.en.us', 'hello');
+	// clientBus1.send('outbound!greeting', 'client1');
+	// clientBus2.send('outbound!greeting', 'client2');
 });
