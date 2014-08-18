d385ddb51f87a59d45ed8e1cfa6031c2a5295486
Jose Badeau <Jose Badeau@JoseBadeau-PC>
2014-08-18 21:28:41 +0200

    .

---
+++ bower_components/metawidget/metawidget-bootstrap.js
@@ -0,0 +1,271 @@
+// Metawidget 3.9
+//
+// This file is dual licensed under both the LGPL
+// (http://www.gnu.org/licenses/lgpl-2.1.html) and the EPL
+// (http://www.eclipse.org/org/documents/epl-v10.php). As a
+// recipient of Metawidget, you may choose to receive it under either
+// the LGPL or the EPL.
+//
+// Commercial licenses are also available. See http://metawidget.org
+// for details.
+
+/**
+ * @author <a href="http://kennardconsulting.com">Richard Kennard</a>
+ */
+
+var metawidget = metawidget || {};
+
+( function() {
+
+	'use strict';
+
+	/**
+	 * @namespace Metawidget Twitter Bootstrap support.
+	 */
+
+	metawidget.bootstrap = metawidget.bootstrap || {};
+
+	/**
+	 * @namespace WidgetProcessors for Twitter Bootstrap environments.
+	 */
+
+	metawidget.bootstrap.widgetprocessor = metawidget.bootstrap.widgetprocessor || {};
+
+	/**
+	 * @class WidgetProcessor to add CSS styles for Bootstrap.
+	 *        <p>
+	 *        Note: in some cases this WidgetProcessor wraps the given widget
+	 *        with Bootstrap-specific markup (e.g. &lt;div
+	 *        class="input-prepend"&gt;). Therefore, BootstrapWidgetProcessor
+	 *        should come <em>after</em> WidgetProcessors that expect widgets
+	 *        to be unwrapped (such as <tt>SimpleBindingProcessor</tt>).
+	 */
+
+	metawidget.bootstrap.widgetprocessor.BootstrapWidgetProcessor = function() {
+
+		if ( ! ( this instanceof metawidget.bootstrap.widgetprocessor.BootstrapWidgetProcessor ) ) {
+			throw new Error( "Constructor called as a function" );
+		}
+	};
+
+	metawidget.bootstrap.widgetprocessor.BootstrapWidgetProcessor.prototype.processWidget = function( widget, elementName, attributes, mw ) {
+
+		var tagName = widget.tagName;
+
+		if ( tagName === 'TABLE' ) {
+
+			metawidget.util.appendToAttribute( widget, 'class', 'table table-striped table-bordered table-hover' );
+
+		} else if ( tagName === 'SELECT' || tagName === 'TEXTAREA' ) {
+
+			metawidget.util.appendToAttribute( widget, 'class', 'form-control' );
+
+		} else if ( tagName === 'OUTPUT' ) {
+
+			// Pad output tags the same way as .form-control pads input tags.
+			// See:
+			// https://github.com/twbs/bootstrap/issues/9969
+
+			metawidget.util.appendToAttribute( widget, 'style', 'padding:6px 12px', ';' );
+
+		} else if ( tagName === 'INPUT' ) {
+
+			var type = widget.getAttribute( 'type' );
+
+			switch ( type ) {
+
+				case 'submit':
+					metawidget.util.appendToAttribute( widget, 'class', 'btn btn-primary' );
+					break;
+
+				case 'button':
+					metawidget.util.appendToAttribute( widget, 'class', 'btn btn-default' );
+					break;
+
+				default: {
+
+					if ( type !== 'checkbox' ) {
+						metawidget.util.appendToAttribute( widget, 'class', 'form-control' );
+					}
+
+					if ( attributes.inputPrepend !== undefined || attributes.inputAppend !== undefined ) {
+						var div = metawidget.util.createElement( mw, 'div' );
+						var span;
+						if ( attributes.inputPrepend !== undefined ) {
+							div.setAttribute( 'class', 'input-prepend input-group' );
+							span = metawidget.util.createElement( mw, 'span' );
+							span.setAttribute( 'class', 'add-on input-group-addon' );
+							span.innerHTML = attributes.inputPrepend;
+							div.appendChild( span );
+						}
+						div.appendChild( widget );
+						if ( attributes.inputAppend !== undefined ) {
+							if ( attributes.inputPrepend !== undefined ) {
+								div.setAttribute( 'class', 'input-prepend input-append input-group' );
+							} else {
+								div.setAttribute( 'class', 'input-append input-group' );
+							}
+							span = metawidget.util.createElement( mw, 'span' );
+							span.setAttribute( 'class', 'add-on input-group-addon' );
+							span.innerHTML = attributes.inputAppend;
+							div.appendChild( span );
+						}
+						return div;
+					}
+				}
+			}
+		}
+
+		return widget;
+	};
+
+	/**
+	 * @namespace Layouts for Twitter Bootstrap environments.
+	 */
+
+	metawidget.bootstrap.layout = metawidget.bootstrap.layout || {};
+
+	/**
+	 * @class Layout to wrap widgets with divs suitable for 'form-vertical' or
+	 *        'form-horizontal' Bootstrap layouts.
+	 *        <p>
+	 *        This Layout extends metawidget.layout.DivLayout. It adds Bootstrap
+	 *        CSS classes such as 'form-group' and 'control-label' to the divs.
+	 * 
+	 * @returns {metawidget.bootstrap.layout.BootstrapDivLayout}
+	 */
+
+	metawidget.bootstrap.layout.BootstrapDivLayout = function( config ) {
+
+		if ( ! ( this instanceof metawidget.bootstrap.layout.BootstrapDivLayout ) ) {
+			throw new Error( "Constructor called as a function" );
+		}
+
+		if ( config === undefined ) {
+			config = {};
+		}
+
+		if ( config.version === 2 ) {
+			if ( config.divStyleClasses === undefined ) {
+				config.divStyleClasses = [ 'control-group', undefined, 'controls' ];
+			}
+			if ( config.labelStyleClass === undefined ) {
+				config.labelStyleClass = 'control-label';
+			}
+		} else {
+			if ( config.divStyleClasses === undefined ) {
+				config.divStyleClasses = [ 'form-group', 'col-sm-2 control-label', 'col-sm-10' ];
+			}
+			if ( config.widgetDivSpanAllClass === undefined ) {
+				config.widgetDivSpanAllClass = 'col-sm-12';
+			}
+			if ( config.widgetDivOffsetClass === undefined ) {
+				config.widgetDivOffsetClass = 'col-sm-offset-2';
+			}
+		}
+
+		var layout = new metawidget.layout.DivLayout( config );
+
+		// If there is no label, Bootstrap 3 requires an explicit grid position
+		// to be set or the widget div will not automatically 'pull right'
+
+		if ( config.version !== 2 ) {
+			var superLayoutWidget = layout.layoutWidget;
+			layout.layoutWidget = function( widget, elementName, attributes, container, mw ) {
+
+				superLayoutWidget.call( this, widget, elementName, attributes, container, mw );
+
+				var outerDiv = container.childNodes[container.childNodes.length - 1];
+				if ( outerDiv.childNodes.length === 1 ) {
+					if ( attributes.title === null ) {
+						outerDiv.childNodes[0].setAttribute( 'class', config.widgetDivSpanAllClass );
+					} else {
+						metawidget.util.appendToAttribute( outerDiv.childNodes[0], 'class', config.widgetDivOffsetClass );
+					}
+				}
+			};
+		}
+
+		return layout;
+	};
+
+	/**
+	 * @class LayoutDecorator to decorate widgets from different sections using
+	 *        Bootstrap tabs.
+	 */
+
+	metawidget.bootstrap.layout.TabLayoutDecorator = function( config ) {
+
+		if ( ! ( this instanceof metawidget.bootstrap.layout.TabLayoutDecorator ) ) {
+			throw new Error( 'Constructor called as a function' );
+		}
+
+		metawidget.layout.createNestedSectionLayoutDecorator( config, this, 'bootstrapTabLayoutDecorator' );
+	};
+
+	metawidget.bootstrap.layout.TabLayoutDecorator.prototype.createSectionWidget = function( previousSectionWidget, section, attributes, container, mw ) {
+
+		var tabs = previousSectionWidget;
+
+		// Whole new tabbed pane?
+
+		var ul, content;
+
+		if ( tabs === undefined ) {
+			tabs = metawidget.util.createElement( mw, 'div' );
+			tabs.setAttribute( 'id', metawidget.util.getId( "property", attributes, mw ) + '-tabs' );
+			tabs.setAttribute( 'class', 'tabs' );
+			ul = metawidget.util.createElement( mw, 'ul' );
+			ul.setAttribute( 'class', 'nav nav-tabs' );
+			tabs.appendChild( ul );
+			content = metawidget.util.createElement( mw, 'div' );
+			content.setAttribute( 'class', 'tab-content' );
+			tabs.appendChild( content );
+			this.getDelegate().layoutWidget( tabs, "property", {
+				wide: "true"
+			}, container, mw );
+
+			mw.bootstrapTabLayoutDecorator = mw.bootstrapTabLayoutDecorator || [];
+			mw.bootstrapTabLayoutDecorator.push( tabs );
+		} else {
+			tabs = previousSectionWidget.parentNode.parentNode;
+		}
+
+		// New Tab
+
+		ul = tabs.childNodes[0];
+		var tabId = tabs.getAttribute( 'id' ) + ( ul.childNodes.length + 1 );
+		var li = metawidget.util.createElement( mw, 'li' );
+		if ( ul.childNodes.length === 0 ) {
+			li.setAttribute( 'class', 'active' );
+		}
+		var a = metawidget.util.createElement( mw, 'a' );
+		a.setAttribute( 'data-toggle', 'tab' );
+		a.setAttribute( 'href', '#' + tabId );
+
+		// If Bootstrap is used with AngularJS, target=_self stops Angular from
+		// rewriting this link:
+		// https://groups.google.com/forum/#!topic/angular/yKv8jXYBsBI
+
+		a.setAttribute( 'target', '_self' );
+		li.appendChild( a );
+		ul.appendChild( li );
+
+		content = tabs.childNodes[1];
+		var tab = metawidget.util.createElement( mw, 'div' );
+		if ( content.childNodes.length === 0 ) {
+			tab.setAttribute( 'class', 'tab-pane active' );
+		} else {
+			tab.setAttribute( 'class', 'tab-pane' );
+		}
+		tab.setAttribute( 'id', tabId );
+		content.appendChild( tab );
+
+		// Tab name
+
+		a.innerHTML = section;
+
+		return tab;
+	};
+
+} )();
---
+++ bower_components/metawidget/metawidget-jqueryui.js
@@ -0,0 +1,423 @@
+// Metawidget 3.9
+//
+// This file is dual licensed under both the LGPL
+// (http://www.gnu.org/licenses/lgpl-2.1.html) and the EPL
+// (http://www.eclipse.org/org/documents/epl-v10.php). As a
+// recipient of Metawidget, you may choose to receive it under either
+// the LGPL or the EPL.
+//
+// Commercial licenses are also available. See http://metawidget.org
+// for details.
+
+var metawidget = metawidget || {};
+
+( function() {
+
+	'use strict';
+
+	/**
+	 * @namespace Metawidget for JQuery UI environments.
+	 */
+
+	metawidget.jqueryui = metawidget.jqueryui || {};
+
+	/**
+	 * @namespace JQuery UI WidgetBuilders.
+	 */
+
+	metawidget.jqueryui.widgetbuilder = metawidget.jqueryui.widgetbuilder || {};
+
+	/**
+	 * @class Builds widgets using JQuery UI.
+	 *        <p>
+	 *        Chooses JQuery UI widgets such as <tt>slider</tt> and
+	 *        <tt>spinner</tt> to suit the inspected fields. Returns undefined
+	 *        for everything else.
+	 */
+
+	metawidget.jqueryui.widgetbuilder.JQueryUIWidgetBuilder = function() {
+
+		if ( ! ( this instanceof metawidget.jqueryui.widgetbuilder.JQueryUIWidgetBuilder ) ) {
+			throw new Error( "Constructor called as a function" );
+		}
+	};
+
+	metawidget.jqueryui.widgetbuilder.JQueryUIWidgetBuilder.prototype.buildWidget = function( elementName, attributes, mw ) {
+
+		// Not for us?
+
+		if ( metawidget.util.isTrueOrTrueString( attributes.readOnly ) ) {
+			return;
+		}
+
+		if ( metawidget.util.isTrueOrTrueString( attributes.hidden ) ) {
+			return;
+		}
+
+		// Number
+
+		if ( attributes.type === 'number' ) {
+
+			if ( attributes.minimum && attributes.maximum ) {
+				var slider = metawidget.util.createElement( mw, 'div' );
+				$( slider ).slider();
+				return slider;
+			}
+
+			var spinner = metawidget.util.createElement( mw, 'input' );
+			$( spinner ).spinner();
+			return $( spinner ).spinner( 'widget' )[0];
+		}
+
+		// Datepicker
+
+		if ( attributes.type === 'date' ) {
+			var date = metawidget.util.createElement( mw, 'input' );
+			$( date ).datepicker();
+			return date;
+		}
+	};
+
+	/**
+	 * @namespace JQuery UI WidgetProcessors.
+	 */
+
+	metawidget.jqueryui.widgetprocessor = metawidget.jqueryui.widgetprocessor || {};
+
+	/**
+	 * @class Binds JQuery UI specific widgets, using the JQuery
+	 *        <tt>$( widget ).foo( 'value', value )</tt> syntax. Clients
+	 *        should still use SimpleBindingProcessor for all non-JQuery UI
+	 *        widgets.
+	 */
+
+	metawidget.jqueryui.widgetprocessor.JQueryUIBindingProcessor = function() {
+
+		if ( ! ( this instanceof metawidget.jqueryui.widgetprocessor.JQueryUIBindingProcessor ) ) {
+			throw new Error( "Constructor called as a function" );
+		}
+	};
+
+	metawidget.jqueryui.widgetprocessor.JQueryUIBindingProcessor.prototype.onStartBuild = function( mw ) {
+
+		mw._jQueryUIBindingProcessorBindings = {};
+	};
+
+	metawidget.jqueryui.widgetprocessor.JQueryUIBindingProcessor.prototype.processWidget = function( widget, elementName, attributes, mw ) {
+
+		var value;
+		var typeAndNames = metawidget.util.splitPath( mw.path );
+		var toInspect = metawidget.util.traversePath( mw.toInspect, typeAndNames.names );
+
+		if ( elementName !== 'entity' && toInspect ) {
+			value = toInspect[attributes.name];
+		} else {
+			value = toInspect;
+		}
+
+		var isBindable = false;
+
+		if ( widget.hasAttribute( 'class' ) ) {
+			var styleClass = widget.getAttribute( 'class' );
+
+			if ( styleClass.indexOf( 'ui-slider' ) !== -1 ) {
+				$( widget ).slider( 'value', value );
+				isBindable = true;
+			} else if ( styleClass.indexOf( 'ui-spinner' ) !== -1 ) {
+				$( widget.childNodes[0] ).spinner( 'value', value );
+				isBindable = true;
+			}
+		}
+
+		if ( isBindable === true || widget.getMetawidget !== undefined ) {
+			mw._jQueryUIBindingProcessorBindings[attributes.name] = widget;
+		}
+
+		return widget;
+	};
+
+	/**
+	 * Save the bindings associated with the given Metawidget.
+	 */
+
+	metawidget.jqueryui.widgetprocessor.JQueryUIBindingProcessor.prototype.save = function( mw ) {
+
+		var typeAndNames = metawidget.util.splitPath( mw.path );
+		var toInspect = metawidget.util.traversePath( mw.toInspect, typeAndNames.names );
+
+		for ( var name in mw._jQueryUIBindingProcessorBindings ) {
+
+			var widget = mw._jQueryUIBindingProcessorBindings[name];
+
+			if ( widget.getMetawidget !== undefined ) {
+				this.save( widget.getMetawidget() );
+				continue;
+			}
+
+			widget = mw.getElement().ownerDocument.getElementById( widget.id );
+
+			var styleClass = widget.getAttribute( 'class' );
+
+			if ( styleClass.indexOf( 'ui-slider' ) !== -1 ) {
+				toInspect[name] = $( widget ).slider( 'value' );
+			} else if ( styleClass.indexOf( 'ui-spinner' ) !== -1 ) {
+				toInspect[name] = $( widget.childNodes[0] ).spinner( 'value' );
+			}
+		}
+	};
+
+	metawidget.jqueryui.layout = metawidget.jqueryui.layout || {};
+
+	/**
+	 * @class LayoutDecorator to decorate widgets from different sections using
+	 *        JQuery UI tabs.
+	 */
+
+	metawidget.jqueryui.layout.TabLayoutDecorator = function( config ) {
+
+		if ( ! ( this instanceof metawidget.jqueryui.layout.TabLayoutDecorator ) ) {
+			throw new Error( "Constructor called as a function" );
+		}
+
+		metawidget.layout.createNestedSectionLayoutDecorator( config, this, 'tabLayoutDecorator' );
+
+		var _superOnEndBuild = this.onEndBuild;
+
+		/**
+		 * Wrap the tabs at the very end, to save using 'tabs.add'.
+		 */
+
+		this.onEndBuild = function( mw ) {
+
+			if ( mw.tabLayoutDecorator !== undefined ) {
+				for ( var loop = 0, length = mw.tabLayoutDecorator.length; loop < length; loop++ ) {
+					$( mw.tabLayoutDecorator[loop] ).tabs();
+				}
+			}
+
+			if ( _superOnEndBuild !== undefined ) {
+				_superOnEndBuild.call( this, mw );
+			}
+		};
+	};
+
+	metawidget.jqueryui.layout.TabLayoutDecorator.prototype.createSectionWidget = function( previousSectionWidget, section, attributes, container, mw ) {
+
+		var tabs = previousSectionWidget;
+
+		// Whole new tabbed pane?
+
+		if ( tabs === undefined ) {
+			tabs = metawidget.util.createElement( mw, 'div' );
+			tabs.setAttribute( 'id', metawidget.util.getId( "property", attributes, mw ) + '-tabs' );
+			tabs.appendChild( metawidget.util.createElement( mw, 'ul' ) );
+			this.getDelegate().layoutWidget( tabs, "property", {
+				wide: "true"
+			}, container, mw );
+
+			mw.tabLayoutDecorator = mw.tabLayoutDecorator || [];
+			mw.tabLayoutDecorator.push( tabs );
+		} else {
+			tabs = previousSectionWidget.parentNode;
+		}
+
+		// New Tab
+
+		var ul = tabs.childNodes[0];
+		var tabId = tabs.getAttribute( 'id' ) + ( ul.childNodes.length + 1 );
+		var li = metawidget.util.createElement( mw, 'li' );
+		var a = metawidget.util.createElement( mw, 'a' );
+		a.setAttribute( 'href', '#' + tabId );
+		a.hash = '#' + tabId;
+		li.appendChild( a );
+		ul.appendChild( li );
+
+		var tab = metawidget.util.createElement( mw, 'div' );
+		tab.setAttribute( 'id', tabId );
+		tabs.appendChild( tab );
+
+		// Tab name
+
+		a.innerHTML = section;
+
+		return tab;
+	};
+
+	/**
+	 * JQuery UI WidgetFactory-based Metawidget.
+	 */
+
+	$.widget( "metawidget.metawidget", {
+
+		/**
+		 * Default configuration
+		 */
+
+		options: {
+			inspector: new metawidget.inspector.PropertyTypeInspector(),
+			widgetBuilder: new metawidget.widgetbuilder.CompositeWidgetBuilder( [ new metawidget.widgetbuilder.OverriddenWidgetBuilder(),
+					new metawidget.jqueryui.widgetbuilder.JQueryUIWidgetBuilder(), new metawidget.widgetbuilder.ReadOnlyWidgetBuilder(), new metawidget.widgetbuilder.HtmlWidgetBuilder() ] ),
+			widgetProcessors: [ new metawidget.widgetprocessor.IdProcessor(), new metawidget.widgetprocessor.RequiredAttributeProcessor(),
+					new metawidget.widgetprocessor.PlaceholderAttributeProcessor(), new metawidget.widgetprocessor.DisabledAttributeProcessor(), new metawidget.jqueryui.widgetprocessor.JQueryUIBindingProcessor(),
+					new metawidget.widgetprocessor.SimpleBindingProcessor() ],
+			layout: new metawidget.layout.HeadingTagLayoutDecorator( new metawidget.layout.TableLayout() )
+		},
+
+		/**
+		 * Constructor
+		 */
+
+		_create: function() {
+
+			// Pipeline (private, based on convention here:
+			// http://forum.jquery.com/topic/what-s-the-right-way-to-store-private-data-in-widget-s-instance)
+
+			this._pipeline = new metawidget.Pipeline( this.element[0] );
+
+			// Configure defaults
+
+			this._pipeline.configure( this.options );
+
+			// First time in, capture the contents of the Metawidget (if any)
+
+			this._overriddenNodes = [];
+
+			var element = this.element[0];
+
+			var mw = this;
+			
+			element.getMetawidget = function() {
+
+				return mw;
+			};
+
+			for ( var loop = 0; loop < element.childNodes.length; ) {
+				if ( element.childNodes[loop].nodeType !== 1 ) {
+					loop++;
+					continue;
+				}
+
+				var childNode = element.childNodes[loop];
+				element.removeChild( childNode );
+				this._overriddenNodes.push( childNode );
+			}
+		},
+
+		/**
+		 * Called when created, and later when changing options.
+		 */
+
+		_refresh: function( inspectionResult ) {
+
+			// Defensive copy
+
+			this.overriddenNodes = [];
+
+			for ( var loop = 0, length = this._overriddenNodes.length; loop < length; loop++ ) {
+				this.overriddenNodes.push( this._overriddenNodes[loop].cloneNode( true ) );
+			}
+
+			// Inspect (if necessary)
+
+			if ( inspectionResult === undefined ) {
+
+				// Safeguard against improperly implementing:
+				// http://blog.kennardconsulting.com/2013/02/metawidget-and-rest.html
+
+				if ( arguments.length > 0 ) {
+					throw new Error( "Calling _refresh( undefined ) may cause infinite loop. Check your argument, or pass no arguments instead" );
+				}
+
+				var splitPath = metawidget.util.splitPath( this.path );
+				inspectionResult = this._pipeline.inspect( this.toInspect, splitPath.type, splitPath.names, this );
+			}
+
+			// Build widgets
+
+			this._pipeline.buildWidgets( inspectionResult, this );
+		},
+
+		/**
+		 * _setOptions is called with a hash of all options that are changing.
+		 */
+
+		_setOptions: function() {
+
+			this._superApply( arguments );
+			this._pipeline.configure( this.options );
+		},
+
+		setReadOnly: function( readOnly ) {
+
+			this.readOnly = readOnly;
+		},
+
+		/**
+		 * Useful for WidgetBuilders to perform nested inspections (eg. for
+		 * Collections).
+		 */
+
+		inspect: function( toInspect, type, names ) {
+
+			return this._pipeline.inspect( toInspect, type, names, this );
+		},
+
+		/**
+		 * Inspect the given toInspect/path and build widgets.
+		 * <p>
+		 * Invoke using
+		 * <tt>$( '#metawidget' ).metawidget( "buildWidgets", toInspect, path )</tt>.
+		 */
+
+		buildWidgets: function( toInspect, path ) {
+
+			if ( toInspect !== undefined ) {
+				this.toInspect = toInspect;
+				this.path = undefined;
+			}
+
+			if ( path !== undefined ) {
+				this.path = path;
+			}
+
+			this._refresh();
+		},
+
+		getWidgetProcessor: function( testInstanceOf ) {
+
+			return this._pipeline.getWidgetProcessor( testInstanceOf );
+		},
+		
+		/**
+		 * Returns the element this Metawidget is attached to.
+		 */
+
+		getElement: function() {
+
+			return this._pipeline.element;
+		},
+
+		buildNestedMetawidget: function( attributes, config ) {
+
+			// Create a 'div' not a 'metawidget', because whilst it's up to the
+			// user what they want their top-level element to be, for browser
+			// compatibility we should stick with something benign for nested
+			// elements
+
+			var nestedWidget = metawidget.util.createElement( this, 'div' );
+
+			// Duck-type our 'pipeline' as the 'config' of the nested
+			// Metawidget. This neatly passes everything down, including a
+			// decremented 'maximumInspectionDepth'
+
+			var nestedMetawidget = $( nestedWidget ).metawidget( this._pipeline );
+
+			nestedMetawidget.metawidget( "setReadOnly", this.readOnly || metawidget.util.isTrueOrTrueString( attributes.readOnly ) );
+			var nestedToInspect = this.toInspect;
+			var nestedPath = metawidget.util.appendPath( attributes, this );
+
+			nestedMetawidget.metawidget( "buildWidgets", nestedToInspect, nestedPath );
+			return nestedWidget;
+		}
+	} );
+} )();
