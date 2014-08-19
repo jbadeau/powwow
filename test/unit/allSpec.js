b388718045771676650ecd3b0990fb8c87eb88b7
Jose Badeau <Jose Badeau@JoseBadeau-PC>
2014-08-19 06:52:09 +0200

    removed console.log

--- test/unit/allSpec.js
+++ test/unit/allSpec.js
@@ -3,7 +3,6 @@ define([ 'powwow' ], function(powwow) {
 	'use strict';
 
 	describe('powwow', function() {
-		console.log("11111111111111111");
 		it('should have a version property', function() {
 			expect(true).toBe(true);
 		});
